e12243cffd175fc933eee8a2f2c766f73636ec45
oladips <oladipupotoheeb75@gmail.com>
2025-02-23 13:26:37 +0100

    currency symbol

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -9,19 +9,19 @@ function Cards({income, expense, totalBalance, showExpenseModal, showIncomeModal
        <Row className='my-row'>
       <Card bordered={true}  className="my-card" >
         <h2 className='mycard'>Current Balance</h2>
-        <p className='mycards'><i class="fa-solid fa-coins"></i> {totalBalance}</p>
+        <p className='mycards'>&#8358; {totalBalance}</p>
         {/* <Button text="" blue={true} /> */}
       </Card>
 
       <Card bordered={true}  className="my-card" >
         <h2>Total Income</h2>
-        <p><i class="fa-solid fa-coins"></i> {income}</p>
+        <p>&#8358; {income}</p>
         <Button text="Add Income" blue={true} onClick={showIncomeModal}/>
       </Card>
 
       <Card bordered={true}  className="my-card" >
         <h2>Total Expenses</h2>
-        <p><i class="fa-solid fa-coins"></i> {expense}</p>
+        <p>&#8358; {expense}</p>
         <Button text="Add Expense" blue={true} onClick={showExpenseModal}/>
       </Card>
     </Row>
